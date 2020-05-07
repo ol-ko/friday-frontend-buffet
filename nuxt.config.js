edb1e0ffe21ac526dcc588facc2bd0025afb13f2
Olga Skurativska <olga.skurativska@homegate.ch>
2020-05-07 21:17:36 +0200

    chore: async fonts fallback for no js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,9 +4,6 @@ export default {
    ** Headers of the page
    */
   head: {
-    htmlAttrs: {
-      lang: 'en'
-    },
     title: 'Friday Frontend BuFFet',
     meta: [
       { charset: 'utf-8' },
@@ -15,32 +12,6 @@ export default {
         hid: 'description',
         name: 'description',
         content: 'A weekly assortment of delicious frontend things to devour'
-      }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'preconnect', href: 'https://www.google-analytics.com' },
-      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-      { rel: 'preconnect', href: 'https://www.googletagmanager.com' },
-      {
-        rel: 'preload',
-        href:
-          'https://fonts.googleapis.com/css?family=Roboto+Slab:400,700&display=swap',
-        as: 'style',
-        onload: 'this.onload=null;this.rel="stylesheet"'
-      },
-      {
-        rel: 'preload',
-        href:
-          'https://fonts.googleapis.com/css?family=Ultra&text=Friday%20Frontend%20BuFFet&display=swap',
-        as: 'style',
-        onload: 'this.onload=null;this.rel="stylesheet"'
-      }
-    ],
-    noscript: [
-      {
-        innerHTML:
-          '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,700&display=swap"><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ultra&text=Friday%20Frontend%20BuFFet&display=swap">'
       }
     ]
   },
