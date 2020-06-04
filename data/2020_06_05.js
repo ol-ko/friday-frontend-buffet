bfef3f912e7422ca6c1b97040fa37d80ca9d6fd6
Olga Skurativska <olga.skurativska@homegate.ch>
2020-06-04 18:56:27 +0200

    edition: 2020-06-05

---
+++ data/2020_06_05.js
@@ -0,0 +1,76 @@
+export default {
+  date: '2020-06-05',
+  links: [
+    {
+      type: 'article',
+      level: 'l',
+      title: 'How to Fully Optimize Webpack 4 Tree Shaking',
+      link:
+        'https://medium.com/@craigmiller160/how-to-fully-optimize-webpack-4-tree-shaking-405e1c76038',
+      author: 'Craig Miller',
+      duration: '15 min read',
+      description: `
+    <p>
+    We tend to think Webpack's tree-shaking feature will make our bundles lean. But looks like it needs some help from us, engineers, to actually work.
+      </p>
+`
+    },
+    {
+      type: 'talk',
+      level: 'm',
+      title: 'Building Resilient Frontend Architecture (GOTO 2019)',
+      link: 'https://www.youtube.com/watch?v=TqfbAXCCVwE',
+      author:
+        'Monica Lent, Software Engineer and Engineering manager, founder of https://affilimate.com/',
+      duration: '33 min',
+      description: `
+    <p>
+       In this talk Monica defines what architecture means for the frontend, dispels some commonly-held myths, and looks at specific tools and techniques on a scale from micro to macro that you can use today to keep your app from turning into that infamous big ball of mud (c)
+      </p>
+      `
+    },
+    {
+      type: 'book',
+      level: 'l',
+      title: 'Modules in JavaScript',
+      link: 'https://exploringjs.com/es6/ch_modules.html',
+      author: 'Dr. Axel Rauschmayer, author of https://2ality.com/',
+      duration: '33 min read',
+      description: `
+    <p>
+        CommonJS? AMD? UMD? ES6 modules? Time to get some clarity and Dr. Rauschmayer is here to help.
+      </p>
+`
+    },
+    {
+      type: 'article',
+      level: 's',
+      title: 'High Performance SVGs',
+      link: 'https://css-tricks.com/high-performance-svgs/',
+      author:
+        'Sarah Drasner, VP of Developer Experience at Netlify, Vue core team member',
+      duration: '7 min read',
+      description: `
+    <p>
+        How to decrease the file size on the SVGs while preserving the appearance and functionality? Here's some really good advice and tools overview. 
+      </p>
+`
+    },
+    {
+      type: 'talk',
+      level: 'm',
+      title:
+        'How to reduce your Vue bundle size using Webpack (Vueconf US 2019)',
+      link: 'https://www.youtube.com/watch?v=NHUiQvYXxw4',
+      author:
+        'Jennifer Bland, Software Engineer @ Stanley Black&Decker, Google Developer Expert',
+      duration: '8 min',
+      description: `
+    <p>
+In this short talk Jennifer demonstrates how to use webpack-bundle-analyzer on a Vue.js app and what could be optimized.
+      </p>
+      `
+    }
+  ],
+  gif: 'https://media.giphy.com/media/uGlLP7Ze3pPAk/giphy.gif'
+}
